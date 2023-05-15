23cd99974a8c061b7c157678ec4cf8b145ef209b
leeceo97 <leeceo97@naver.com>
2023-05-15 22:33:58 +0900

    (feat)S3 deploy settings

--- config/settings/deploy.py
+++ config/settings/deploy.py
@@ -8,6 +8,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 ALLOWED_HOSTS = ['*']
 
+INSTALLED_APPS += [
+    'storages',
+]
 
 DATABASES = {
     'default': {
@@ -22,5 +25,15 @@ DATABASES = {
 
 WSGI_APPLICATION = 'config.wsgi.deploy.application'
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_REGION = 'ap-northeast-2'
+
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.%s.amazonaws.com' % (AWS_STORAGE_BUCKET_NAME, AWS_REGION)
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+
+STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
