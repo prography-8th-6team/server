659063bcb79898da2e8870e6e0a556e816c2ab9b
leeceo97 <leeceo97@naver.com>
2023-05-15 22:52:23 +0900

    (fix)S3 settings error

--- config/settings/deploy.py
+++ config/settings/deploy.py
@@ -36,4 +36,5 @@ AWS_S3_OBJECT_PARAMETERS = {
 }
 
 STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
-STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
