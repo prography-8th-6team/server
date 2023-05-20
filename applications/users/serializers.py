be77c4538f877a34d73252ea641112e31971dc30
o-ddong <odh0112@naver.com>
2023-05-20 20:21:44 +0900

    Feat: User serializer 추가

---
+++ applications/users/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+
+from applications.users.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    nickname = serializers.CharField(required=False)
+    social_id = serializers.CharField(required=False)
+
+    class Meta:
+        model = User
+        fields = ('nickname', 'fcm_token')
