0ce67b913b40cb24387dddb97b0907b29093f441
o-ddong <odh0112@naver.com>
2023-05-22 01:25:35 +0900

    Fix: nickname 랜덤 기능 제거

--- applications/users/utils.py
+++ applications/users/utils.py
@@ -1,28 +1,24 @@
 import requests
 
 
-def kakaoGetUserInfo(access_token):
+def kakao_get_user_info(access_token):
     """
     카카오 사용자 정보 가져오는 함수입니다.
     """
     kakao_url = "https://kapi.kakao.com"
-    getKakaoUserInfoPath = "/v2/user/me"
+    get_kakao_user_info_path = "/v2/user/me"
 
     headers = {"Authorization": f"Bearer ${access_token}",
                "Content-type": "application/x-www-form-urlencoded;charset=utf-8"}
 
-    response = requests.get(kakao_url + getKakaoUserInfoPath, headers=headers)
+    response = requests.get(kakao_url + get_kakao_user_info_path, headers=headers)
 
     try:
-        kakaoInfo = response.json()
-        kakaoId = kakaoInfo['id']
-        nickname = kakaoInfo.get('kakao_account', {}).get('profile', {}).get('nickname', None)
-        import random
-        nickname = nickname if nickname else 'jerny' + ''.join(random.choices('abcdefghijklmnopqrstuvwxyz', k=3))
+        kakao_data = response.json()
+        kakao_id = kakao_data['id']
+        nickname = kakao_data['nickname']
 
-        kakaoInfo = {"id": kakaoId, "nickname": nickname}
-        return kakaoInfo
-    except Exception:
+        kakao_info = {"id": kakao_id, "nickname": nickname}
+        return kakao_info
+    except:
         return None
-
-
