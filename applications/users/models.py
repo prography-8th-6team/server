04a90f8032eff1d85154581c5ae642afcd324142
o-ddong <odh0112@naver.com>
2023-05-22 01:25:14 +0900

    Fix: User Model nickname unique 설정 제거

--- applications/users/models.py
+++ applications/users/models.py
@@ -8,23 +8,18 @@ from applications.base.models import BaseAdminModel
 class UserManager(BaseUserManager):
     use_in_migrations = True
 
-    def create_user(self, nickname, password=None, **kwargs):
-
-        if not nickname:
-            raise ValueError('must have user nickname')
+    def create_user(self, password=None, **kwargs):
 
         user = self.model(
-            nickname=nickname,
             **kwargs,
         )
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, nickname, password=None, **kwargs):
+    def create_superuser(self, password=None, **kwargs):
 
         user = self.create_user(
-            nickname=nickname,
             password=password,
             **kwargs,
         )
@@ -41,13 +36,13 @@ class User(AbstractBaseUser, BaseAdminModel):
     # TODO : social id = 숫자로 할 것인지, 문자로 할 것인지 (카카오는 숫자 대충 10자리, 네이버는 문자열)
     objects = UserManager()
 
-    nickname = models.CharField(max_length=255, unique=True, verbose_name="별명")
+    nickname = models.CharField(max_length=255, verbose_name="별명")
     social_id = models.CharField(max_length=255, verbose_name="소셜 로그인 아이디")
     email = models.CharField(max_length=255, unique=True, null=True, verbose_name="이메일")
     fcm_token = models.CharField(max_length=255, null=True, verbose_name="FCM Token")
     is_admin = models.BooleanField(default=False, verbose_name="관리자 권한")
 
-    USERNAME_FIELD = 'nickname'
+    USERNAME_FIELD = 'email'
 
     def __str__(self):
         return self.nickname
@@ -60,3 +55,4 @@ class User(AbstractBaseUser, BaseAdminModel):
 
     def has_module_perms(self, app_label):
         return self.is_admin
+
