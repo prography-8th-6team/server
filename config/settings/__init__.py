4630671059f7c754c443a643e7fb7db4915c9a33
o-ddong <odh0112@naver.com>
2023-05-27 00:52:24 +0900

    Feat: JWT middleware 예외처리 추가

--- config/settings/__init__.py
+++ config/settings/__init__.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # "applications.base.middleware.JsonWebTokenMiddleWare"
+    "applications.base.middleware.JsonWebTokenMiddleWare"
 ]
 
 ROOT_URLCONF = 'config.urls'
