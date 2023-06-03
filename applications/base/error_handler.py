041adeedc8a5ef78daa1fa5e4cd81bd659d9ecd4
o-ddong <odh0112@naver.com>
2023-06-03 23:27:06 +0900

    Feat: Error Logging 추가

--- applications/base/error_handler.py
+++ applications/base/error_handler.py
@@ -1,12 +1,19 @@
+import logging
+
+
 from django.db import close_old_connections
 from rest_framework.views import exception_handler
 from applications.base.response import operation_failure
+
+logger = logging.getLogger('error')
 
 
 def server_error_handler(exc, context):
     response = exception_handler(exc, context)
     if response:
         return response
-    close_old_connections()
+    logger.exception(exc)
+    logger.error(exc)
     print(exc)
+    close_old_connections()
     return operation_failure
--- config/settings/local.py
+++ config/settings/local.py
@@ -16,3 +16,23 @@ DATABASES = {
 WSGI_APPLICATION = 'config.wsgi.develop.application'
 
 STATIC_URL = 'static/'
+
+DEVELOP_LOG_DIR = '/log/'
+
+LOGGING['handlers'] = {
+    'file': {
+        'level': 'INFO',
+        'class': 'logging.handlers.RotatingFileHandler',
+        'filename': f'{DEVELOP_LOG_DIR}jerny.log',
+        'maxBytes': 10*1024*1024,  # 10MB
+        'backupCount': 5,
+        'formatter': 'verbose'
+    },
+    'error': {
+        'class': 'logging.handlers.RotatingFileHandler',
+        'filename': f'{DEVELOP_LOG_DIR}/error.log',
+        'maxBytes': 10 * 1024 * 1024,  # 10MB
+        'backupCount': 5,
+        'formatter': 'verbose'
+    }
+}
