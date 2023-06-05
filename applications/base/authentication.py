4219cc20271f6dc09492b128422b98300b1c3c66
o-ddong <odh0112@naver.com>
2023-06-05 16:23:16 +0900

    Feat: authentication User 미존재 시 예외처리 추가

--- applications/base/authentication.py
+++ applications/base/authentication.py
@@ -1,4 +1,5 @@
 from rest_framework.authentication import BaseAuthentication
+from rest_framework.exceptions import AuthenticationFailed
 
 from applications.base.jwt_utils import decode_jwt
 from applications.users.models import User
@@ -7,18 +8,18 @@ from applications.users.models import User
 class JsonWebTokenAuthentication(BaseAuthentication):
 
     def authenticate(self, request):
+        message = {"message": "INVALID_TOKEN"}
         access_token = request.headers.get("Authorization", None)
         if not access_token:
             return None
 
         auth_type, token = access_token.split(' ')
         payload = decode_jwt(token)
-        if auth_type == "Bearer":
-            user_id = payload.get("user_id", None)
-            try:
-                user = User.objects.get(id=user_id)
-                return user, None
-            except User.DoesNotExist:
-                return None
-        else:
-            return None
+
+        user_id = payload.get("user_id", None)
+        try:
+            user = User.objects.get(id=user_id)
+            return user, None
+        except User.DoesNotExist:
+            raise AuthenticationFailed(message)
+
