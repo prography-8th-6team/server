9ec6710f6a43bbc118fb72dea5eeed15a23a82b5
o-ddong <odh0112@naver.com>
2023-06-18 15:02:29 +0900

    Feat: djdt 기능 추가

--- config/urls.py
+++ config/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import debug_toolbar
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, re_path, include
@@ -60,4 +61,8 @@ if settings.DEBUG:
        re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
        re_path(r'^swagger', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
        re_path(r'^redoc', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    ]
+
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls)),
     ]
