452e6cfdfc9c94833eb14d6fd9032c2efc61252f
oscar <oscar.lee@velymonkeys.com>
2023-06-21 08:21:14 +0900

    (fix)billing status

--- applications/billings/models.py
+++ applications/billings/models.py
@@ -11,11 +11,20 @@ class Billing(BaseMoneyModel):
     title = models.CharField(max_length=31)
     image = models.ImageField()
     category = models.CharField(max_length=15, choices=BillingLineCategory.CHOICES, default=BillingLineCategory.FOOD)
+    status = models.CharField(max_length=31, choices=SettlementStatus.CHOICES, default=SettlementStatus.NOT_CHARGED)
     paid_by = models.ForeignKey(User, related_name='billing_lines', on_delete=models.CASCADE)
     paid_date = models.DateField()
+
+    @property
+    def remaining_amount(self):
+        return self.total_amount.amount - self.captured_amount.amount
 
 
 class Settlement(BaseMoneyModel):
     billing = models.ForeignKey(Billing, related_name='settlements', on_delete=models.CASCADE)
     user = models.ForeignKey(User, related_name='settlements', on_delete=models.CASCADE)
     status = models.CharField(max_length=31, choices=SettlementStatus.CHOICES, default=SettlementStatus.NOT_CHARGED)
+
+    @property
+    def remaining_amount(self):
+        return self.total_amount.amount - self.captured_amount.amount
